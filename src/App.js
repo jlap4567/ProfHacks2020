124e9bef1eed9828f1909afb91c2df18b6ac23e7
Eddy Yeung <yeung.edward36@gmail.com>
2020-02-29 16:15:08 -0500

    form almost done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import MainPage from "./pages/MainPage";
-import Form from "./components/Form"
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <MainPage/>
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 import axios from "axios";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import Grid from "@material-ui/core/Grid";
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
+import TextField from "@material-ui/core/TextField";
 
 export default class TaskForm extends Component {
   constructor(props) {
     super(props);
 
-    this.onChangeName_of_Restaurant = this.onChangeName_of_Restaurant.bind(this);
+    this.onChangeName_of_Restaurant = this.onChangeName_of_Restaurant.bind(
+      this
+    );
     this.onChangeLocation = this.onChangeLocation.bind(this);
-    this.onChangeFood_available_time = this.onChangeFood_available_time.bind(this);
-    this.onChangeRecurring = this.onChangeRecurring.bind(this);
-    this.onChangeRecurring_Time = this.onChangeRecurring_Time.bind(this);
+    this.onChangefood_available_start_time = this.onChangefood_available_start_time.bind(
+      this
+    );
+    // this.onChangeRecurring = this.onChangeRecurring.bind(this);
+    // this.onChangeRecurring_Time = this.onChangeRecurring_Time.bind(this);
     this.onChangeFood_Available = this.onChangeFood_Available.bind(this);
-    this.onChangePontential_Allergies = this.onChangePontential_Allergies.bind(this);
+    this.onChangePontential_Allergies = this.onChangePontential_Allergies.bind(
+      this
+    );
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       name_of_restaurant: "",
       location: "",
-      food_available_time: "",
-      recurring: false,
-      recurring_time: "",
+      food_available_start_time: "07:30",
+      food_available_end_time: "10:30",
       food_available: "",
       potential_allergies: ""
     };
@@ -39,27 +51,31 @@ export default class TaskForm extends Component {
       location: e.target.value
     });
   }
-  onChangeFood_available_time(date) {
+  onChangefood_available_start_time(e) {
     this.setState({
-      food_available_time: date
+      food_available_start_time: e.target.value
     });
   }
-  onChangeRecurring(e) {
-
-    console.log("hey");
-    if (e.target.value === "1"){
-        e.target.value = true;
-    }
-    console.log(e.target.value)
+  onChangefood_available_end_time(e) {
     this.setState({
-      required: e.target.value
+      food_available_end_time: e.target.value
     });
   }
-  onChangeRecurring_Time(date) {
-    this.setState({
-      recurring_time: date
-    });
-  }
+  //   onChangeRecurring(e) {
+  //     console.log("hey");
+  //     if (e.target.value === "1") {
+  //       e.target.value = true;
+  //     }
+  //     console.log(e.target.value);
+  //     this.setState({
+  //       required: e.target.value
+  //     });
+  //   }
+  //   onChangeRecurring_Time(date) {
+  //     this.setState({
+  //       recurring_time: date
+  //     });
+  //   }
   onChangeFood_Available(e) {
     this.setState({
       food_available: e.target.value
@@ -78,9 +94,8 @@ export default class TaskForm extends Component {
     const donor = {
       name_of_restaurant: this.state.name_of_restaurant,
       location: this.state.location,
-      food_available_time: this.state.food_available_time,
-      recurring: this.state.recurring,
-      recurring_time: this.state.recurring_time,
+      food_available_start_time: this.state.food_available_start_time,
+      food_available_end_time: this.state.food_available_end_time,
       food_available: this.state.food_available,
       potential_allergies: this.state.potential_allergies
     };
@@ -96,46 +111,82 @@ export default class TaskForm extends Component {
 
   render() {
     return (
-        <div className="formStyle">
-      <Form className="form" onSubmit={this.onSubmit}>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" value={this.state.name_of_restaurant}
-              onChange={this.onChangeName_of_Restaurant}/>
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlInput2">
-          <Form.Label>Location</Form.Label>
-          <Form.Control type="text" value={this.state.location}
-              onChange={this.onChangeLocation}/>
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Food Avail Time</Form.Label>
-          <Form.Control type="text" value={this.state.food_available_time}
-              onChange={this.onChangeFood_available_time}/>
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlSelect1">
-          <Form.Label>Recurring Time</Form.Label>
-          <Form.Control type="number" value={this.state.recurring_time}
-              onChange={this.onChangeRecurring_Time}/>
-        </Form.Group>
-        <Form.Group id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Required?" value="1"
-              onChange={this.onChangeRecurring}/>
-        </Form.Group>
-        <Form.Group controlId="formGridState">
-          <Form.Label>Food Available</Form.Label>
-          <Form.Control type="text" value={this.state.food_available}
-              onChange={this.onChangeFood_Available}>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group id="formGridCheckbox">
-          <label>Date : </label>
-          <DatePicker selected={this.state.potential_allergies} onChange={this.onChangePontential_Allergies} />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <div className="formStyle">
+        <Form className="form" onSubmit={this.onSubmit}>
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.name_of_restaurant}
+              onChange={this.onChangeName_of_Restaurant}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlInput2">
+            <Form.Label>Location</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.location}
+              onChange={this.onChangeLocation}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Food Avail Start Time</Form.Label>
+            <form noValidate>
+              <TextField
+                id="time"
+                label="Alarm clock"
+                type="time"
+                defaultValue="07:30"
+                value={this.state.food_available_start_time}
+                onChange={this.onChangefood_available_start_time}
+                InputLabelProps={{
+                  shrink: true
+                }}
+                inputProps={{
+                  step: 300 // 5 min
+                }}
+              />
+            </form>
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Food Avail End Time</Form.Label>
+            <form noValidate>
+              <TextField
+                id="time"
+                label="Alarm clock"
+                type="time"
+                defaultValue="07:30"
+                value={this.state.food_available_end_time}
+                onChange={this.onChangefood_available_end_time}
+                InputLabelProps={{
+                  shrink: true
+                }}
+                inputProps={{
+                  step: 300 // 5 min
+                }}
+              />
+            </form>
+          </Form.Group>
+          <Form.Group controlId="formGridState">
+            <Form.Label>Food Available</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.food_available}
+              onChange={this.onChangeFood_Available}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Potential Allergies</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.potential_allergies}
+              onChange={this.onChangePontential_Allergies}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     );
   }
